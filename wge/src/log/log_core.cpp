52f49647e6223fd8a83d467bb55f606dd8b11682
zie <zie.develop@gmail.com>
2021-05-02 12:48:22 +0200

    add simple wge logging

---
+++ wge/src/log/log_core.cpp
@@ -0,0 +1,34 @@
+#include "wge/log/log_core.hpp"
+#include "wge/string/view.hpp"
+
+#include <fstream>
+#include <iostream>
+
+namespace wge {
+namespace log {
+namespace detail {
+
+inline void log_to_stdout(wge::string_view msg) noexcept { std::cout << msg << '\n'; }
+
+inline void log_to_file(wge::string_view msg) noexcept {
+    std::ofstream file("wge_log.txt", std::ios_base::app);
+    file << msg << '\n';
+}
+
+}  // namespace detail
+}  // namespace log
+}  // namespace wge
+
+namespace wge {
+namespace log {
+
+void write_msg(wge::string_view msg) noexcept {
+#if defined(WOTH_PLATFORM_PSP)
+    detail::log_to_file(msg);
+#else
+    detail::log_to_stdout(msg);
+#endif
+}
+
+}  // namespace log
+}  // namespace wge

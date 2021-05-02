40608f5c2809c121e4a2ab9f254e34bcf34b5477
zie <zie.develop@gmail.com>
2021-05-02 13:59:24 +0200

    updated android ndk to r21b

--- wge/include/wge/string/view.hpp
+++ wge/include/wge/string/view.hpp
@@ -1,7 +1,14 @@
 #ifndef WOTH_WGE_STRING_VIEW_HPP
 #define WOTH_WGE_STRING_VIEW_HPP
 
-#if __cplusplus >= 201703L
+#if defined(__has_include) 
+    #define WGE_HAS_INCLUDE(x) __has_include(x)
+#else
+    #define WGE_HAS_INCLUDE(x) 0
+#endif
+
+#if (WGE_HAS_INCLUDE(<string_view>) && (__cplusplus >= 201703L || defined(_LIBCPP_VERSION)))
+
     #include <string_view>
 
 namespace wge {
@@ -11,7 +18,7 @@ using basic_string_view = std::basic_string_view<Char>;
 using string_view = basic_string_view<char>;
 }  // namespace wge
 
-#elif __cplusplus >= 201402L
+#elif WGE_HAS_INCLUDE("experimental/string_view") && __cplusplus >= 201402L
 
     #include <experimental/string_view>
 

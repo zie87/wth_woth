3ae7c4158b035fee046bc97da7cef31f319d50eb
zie <zie.develop@gmail.com>
2021-06-08 21:25:06 +0200

    fix wrong path for android build

--- projects/mtg/Android/jni/Android.mk
+++ projects/mtg/Android/jni/Android.mk
@@ -133,7 +133,7 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
         $(MTG_PATH)/src/WFont.cpp \
         $(MTG_PATH)/src/WGui.cpp \
         $(MTG_PATH)/src/WResourceManager.cpp \
-        $(WGE_PATH)/src/video/image_loader.cpp \
+        $(WGE_PATH)/src/wge/video/image_loader.cpp \
         $(JGE_PATH)/src/SDLmain.cpp \
         $(JGE_PATH)/src/Encoding.cpp \
         $(JGE_PATH)/src/JAnimator.cpp \

d005bfcd94746f24b3522fc45d3241b1d0dcdc31
Tobias Zindl <zie.develop@gmail.com>
2021-06-30 14:01:25 +0200

    add cpack integration

--- .github/tools/psp/build.sh
+++ .github/tools/psp/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
 
 echo PSPDEV = $PSPDEV
 echo psp-config = `psp-config --psp-prefix`
 
 cmake -S. -Bbuild_psp -DCMAKE_BUILD_TYPE=$1 -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchains/pspsdk-psp.cmake
 cmake --build build_psp -- -j $(nproc)
+cmake --build build_psp --target package
 
 

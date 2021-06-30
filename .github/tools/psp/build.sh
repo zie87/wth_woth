deefd5986048e8db1a02df0d950896b2bc0dad1c
zie <zie.develop@gmail.com>
2021-06-30 20:24:44 +0200

    add exception.prx to psp package

--- .github/tools/psp/build.sh
+++ .github/tools/psp/build.sh
@@ -4,6 +4,10 @@ set -e
 echo PSPDEV = $PSPDEV
 echo psp-config = `psp-config --psp-prefix`
 
+cd extern/psp_exception_handler/prx
+make -j $(nproc)
+cd -
+
 cmake -S. -Bbuild_psp -DCMAKE_BUILD_TYPE=$1 -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchains/pspsdk-psp.cmake
 cmake --build build_psp -- -j $(nproc)
 cmake --build build_psp --target package

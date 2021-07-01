2f142df32718d5489cf160663b5ffda164104ee1
zie87 <zie.develop@gmail.com>
2021-07-01 17:06:25 +0200

    Span (#46)
    * add span implementation
    * add to_address

---
+++ wge/include/wge/span.hpp
@@ -0,0 +1,131 @@
+#ifndef WOTH_WTH_SPAN_HPP
+#define WOTH_WTH_SPAN_HPP
+
+#include <wge/types.hpp>
+#include <wge/debug/assert.hpp>
+#include <wge/memory.hpp>
+
+#include <iterator>
+#include <type_traits>
+#include <limits>
+#include <array>
+
+namespace wge {
+inline constexpr wge::size_t dynamic_extent = std::numeric_limits<wge::size_t>::max();
+
+template <typename T, wge::size_t Extent = wge::dynamic_extent>
+class span final {
+public:
+    using element_type = T;
+    using value_type   = std::remove_cv_t<T>;
+    using size_type    = wge::size_t;
+
+    using pointer         = T*;
+    using const_pointer   = const T*;
+    using reference       = T&;
+    using const_reference = const T&;
+
+    using iterator         = pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+
+    static constexpr size_type extent = Extent;
+
+    constexpr span() noexcept            = default;
+    constexpr span(const span&) noexcept = default;
+
+    template <class It>
+    constexpr span(It f, size_type count) noexcept : m_data(wge::to_address(f)), m_size(count) {}
+
+    template <class It, class End>
+    constexpr span(It f, End l) noexcept : span(f, static_cast<size_type>(std::distance(f, l))) {}
+
+    template <size_t N>
+    explicit constexpr span(element_type (&arr)[N]) noexcept : span(arr, N) {}
+
+    template <size_t N>
+    explicit constexpr span(std::array<value_type, N>& arr) noexcept : span(arr.data(), N) {}
+
+    template <size_t N>
+    explicit constexpr span(const std::array<value_type, N>& arr) noexcept : span(arr.data(), N) {}
+
+    constexpr span& operator=(const span&) noexcept = default;
+
+    constexpr size_type size() const noexcept { return m_size; }
+    constexpr size_type size_byte() const noexcept { return m_size * sizeof(element_type); }
+
+    [[nodiscard]] constexpr bool empty() const noexcept { return (size() == 0U); }
+
+    constexpr iterator begin() const noexcept { return data(); }
+    constexpr iterator end() const noexcept { return data() + size(); }
+
+    constexpr reverse_iterator rbegin() const noexcept { return reverse_iterator(end()); }
+    constexpr reverse_iterator rend() const noexcept { return reverse_iterator(begin()); }
+
+    constexpr pointer data() const noexcept { return m_data; }
+
+    constexpr reference front() const { return *begin(); }
+    constexpr reference back() const { return *(end() - 1); }
+    constexpr reference operator[](size_type idx) const { return data()[idx]; }
+
+    template <wge::size_t Count>
+    constexpr auto first() const noexcept -> span<element_type, Count> {
+        wge_expects(Count <= size());
+        return {data(), Count};
+    }
+    constexpr auto first(size_type count) const noexcept -> span<element_type, wge::dynamic_extent> {
+        wge_expects(count <= size());
+        return {data(), count};
+    }
+
+    template <wge::size_t Count>
+    constexpr auto last() const noexcept -> span<element_type, Count> {
+        wge_expects(Count <= size());
+        return {data() + size() - Count, Count};
+    }
+    constexpr auto last(size_type count) const noexcept -> span<element_type, wge::dynamic_extent> {
+        wge_expects(count <= size());
+        return {data() + size() - count, count};
+    }
+
+    template <size_t Offset, size_t Count = wge::dynamic_extent>
+    constexpr span<element_type, Count> subspan() const noexcept {
+        wge_expects(Offset <= size());
+        wge_expects(Count == wge::dynamic_extent || Count <= size());
+        constexpr size_type cnt = (Count == wge::dynamic_extent ? size() - Offset : Count);
+        return span<element_type, Count>{data() + Offset, cnt};
+    }
+
+    constexpr span<element_type, wge::dynamic_extent> subspan(size_type offset,
+                                                              size_type count = wge::dynamic_extent) const noexcept {
+        wge_expects(offset <= size());
+        wge_expects(count <= size() || count == wge::dynamic_extent);
+
+        if (count == dynamic_extent) {
+            return {data() + offset, size() - offset};
+        }
+
+        wge_expects(count <= size() - offset);
+        return {data() + offset, count};
+    }
+
+private:
+    pointer m_data   = nullptr;
+    size_type m_size = 0U;
+};
+
+template <class T, wge::size_t N>
+auto as_bytes(wge::span<T, N> s) noexcept
+    -> wge::span<const wge::byte_t, (N == wge::dynamic_extent) ? wge::dynamic_extent : N * sizeof(T)> {
+    return {reinterpret_cast<const wge::byte_t*>(s.data()), s.size_byte()};
+}
+
+template <class T, wge::size_t N>
+auto as_writable_bytes(wge::span<T, N> s) noexcept
+    -> std::enable_if_t<!std::is_const_v<T>,
+                        wge::span<wge::byte_t, (N == wge::dynamic_extent) ? wge::dynamic_extent : N * sizeof(T)>> {
+    return {reinterpret_cast<wge::byte_t*>(s.data()), s.size_byte()};
+}
+
+}  // namespace wge
+
+#endif  // WOTH_WTH_SPAN_HPP
---
+++ wge/tests/wge/span_tests.cpp
@@ -0,0 +1,158 @@
+#include <wge/span.hpp>
+
+#include <wtest/wtest.hpp>
+
+#include <algorithm>
+#include <numeric>
+
+WTEST_SUITE(WgeSpan);
+
+WTEST_SETUP(WgeSpan) {}
+WTEST_TEAR_DOWN(WgeSpan) {}
+
+WTEST_CASE(WgeSpan, default_construction) {
+    wge::span<wge::u16, 10U> test_span{};
+    WTEST_ASSERT_TRUE(test_span.empty());
+    WTEST_ASSERT_EQUAL(static_cast<std::size_t>(0U), test_span.size());
+}
+
+WTEST_CASE(WgeSpan, construct_from_array) {
+    constexpr wge::size_t array_size = 25;
+    wge::byte_t test_array[array_size];
+
+    wge::span<wge::byte_t> test_span(test_array);
+    WTEST_ASSERT_EQUAL(array_size, test_span.size());
+}
+
+WTEST_CASE(WgeSpan, construct_from_array_class) {
+    {
+        constexpr wge::size_t array_size = 5U;
+        const std::array<wge::u32, array_size> test_array{};
+        wge::span<const wge::u32> test_span(test_array);
+
+        WTEST_ASSERT_FALSE(test_span.empty());
+        WTEST_ASSERT_EQUAL(array_size, test_span.size());
+
+        constexpr wge::size_t byte_size = test_array.size() * sizeof(decltype(test_array)::value_type);
+        WTEST_ASSERT_EQUAL(byte_size, test_span.size_byte());
+    }
+
+    {
+        constexpr wge::size_t array_size = 10U;
+        std::array<wge::f32, array_size> test_array{};
+        wge::span<const wge::f32> test_span(test_array);
+
+        WTEST_ASSERT_FALSE(test_span.empty());
+        WTEST_ASSERT_EQUAL(array_size, test_span.size());
+    }
+}
+
+WTEST_CASE(WgeSpan, iterators_and_algorithms) {
+    {
+        constexpr std::array test_array{1, 2, 3, 4, 5, 6};
+        using span_type = wge::span<const decltype(test_array)::value_type, test_array.size()>;
+        span_type test_span{test_array};
+
+        const auto value = std::accumulate(test_span.begin(), test_span.end(), 0);
+        WTEST_ASSERT_EQUAL(21, value);
+    }
+    {
+        constexpr std::array test_array{1, 2, 3, 4, 5, 6};
+        using span_type = wge::span<const decltype(test_array)::value_type, test_array.size()>;
+        span_type test_span{test_array};
+
+        const auto it = std::max_element(test_span.rbegin(), test_span.rend());
+        WTEST_ASSERT_EQUAL(test_span.back(), *it);
+    }
+}
+
+WTEST_CASE(WgeSpan, subspan_getter) {
+    using value_type                 = wge::i16;
+    constexpr wge::size_t array_size = 10;
+
+    using array_type = std::array<value_type, array_size>;
+    using span_type  = wge::span<array_type::value_type, array_size>;
+
+    constexpr array_type expected{1, 1, 1, 2, 2, 2, 2, 3, 3, 3};
+
+    {
+        array_type test_array{};
+        span_type test_span(test_array.begin(), test_array.end());
+
+        {
+            auto first_part = test_span.first(3);
+            std::fill(first_part.begin(), first_part.end(), 1);
+        }
+
+        {
+            auto middle_part = test_span.subspan(3, 4);
+            std::fill(middle_part.rbegin(), middle_part.rend(), 2);
+        }
+
+        {
+            auto last_part = test_span.last(3);
+            std::fill(last_part.rbegin(), last_part.rend(), 3);
+        }
+
+        const auto span_cpy = test_span.subspan(0);
+        for (wge::size_t idx = 0; idx < array_size; ++idx) {
+            WTEST_ASSERT_EQUAL(expected[idx], span_cpy[idx]);
+        }
+    }
+
+    {
+        array_type test_array{};
+        span_type test_span(test_array.begin(), test_array.end());
+
+        {
+            auto first_part = test_span.first<3>();
+            std::fill(first_part.begin(), first_part.end(), 1);
+        }
+
+        {
+            auto middle_part = test_span.subspan<3, 4>();
+            std::fill(middle_part.rbegin(), middle_part.rend(), 2);
+        }
+
+        {
+            auto last_part = test_span.last<3>();
+            std::fill(last_part.rbegin(), last_part.rend(), 3);
+        }
+
+        for (wge::size_t idx = 0; idx < array_size; ++idx) {
+            WTEST_ASSERT_EQUAL(expected[idx], test_span[idx]);
+        }
+    }
+}
+
+WTEST_CASE(WgeSpan, span_as_bytes) {
+    {
+        constexpr wge::size_t array_size = 5U;
+        const std::array<wge::u32, array_size> test_array{};
+
+        constexpr wge::size_t byte_size = array_size * sizeof(wge::u32);
+
+        const auto byte_span = wge::as_bytes( wge::span<const wge::u32>(test_array) );
+        WTEST_ASSERT_EQUAL(byte_size, byte_span.size() );
+    }
+
+    {
+        constexpr wge::size_t array_size = 5U;
+        std::array<wge::u32, array_size> test_array{};
+
+        constexpr wge::size_t byte_size = array_size * sizeof(wge::u32);
+
+        const auto byte_span = wge::as_writable_bytes( wge::span<wge::u32, array_size>(test_array) );
+        WTEST_ASSERT_EQUAL(byte_size, byte_span.size() );
+    }
+}
+
+
+WTEST_SUITE_RUNNER(WgeSpan) {
+    RUN_TEST_CASE(WgeSpan, default_construction);
+    RUN_TEST_CASE(WgeSpan, construct_from_array);
+    RUN_TEST_CASE(WgeSpan, construct_from_array_class);
+    RUN_TEST_CASE(WgeSpan, iterators_and_algorithms);
+    RUN_TEST_CASE(WgeSpan, subspan_getter);
+    RUN_TEST_CASE(WgeSpan, span_as_bytes);
+}

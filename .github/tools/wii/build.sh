e79f855419f85041b91b1ef460211d1940013c83
Tobias Zindl <zie.develop@gmail.com>
2021-10-22 12:26:37 +0200

    add workflow for wii

--- .github/tools/wii/build.sh
+++ .github/tools/wii/build.sh
@@ -4,4 +4,5 @@ echo DEVKITPRO = $DEVKITPRO
 echo DEVKITPPC = $DEVKITPPC
 
 cmake -H. -Bbuild_wii -DCMAKE_BUILD_TYPE=$1 -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchains/devkit-wii.cmake
-cmake --build build_wii -- -j $(nproc)
+# cmake --build build_wii -- -j $(nproc)
+cmake --build build_wii --target wge -- -j $(nproc) 
